23e900daa1b75d575e5008292b790b127337b1c2
MingChen <asd831129@gmail.com>
2025-10-17 09:04:18 -0500

    build: update docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NBA-2526-Preseason-Player-Rating/', // ← 一定要和 repo 名稱一致
+  base: '/NBA-2526-Preseason-Player-Rating/', // 必須和 repo 名稱一致
 })
