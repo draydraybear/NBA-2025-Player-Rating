ddf18dd30bc902fec1321f2d3658aa134082b2b2
MingChen <asd831129@gmail.com>
2025-10-17 09:29:32 -0500

    build: fix Vite index.html entry and rebuild docs

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
+// src/main.jsx
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css'   // 若你有 Tailwind 就保留這行；沒有也不影響
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
