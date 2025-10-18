19bfa585d3d5a81a25af034ca3000b439724f0dd
MingChen <asd831129@gmail.com>
2025-10-17 21:10:47 -0500

    build: update docs for pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NBA-2025-Player-Rating/', // ← 你的 repo 名稱
+  base: '/NBA-2025-Player-Rating/',   // ★很重要（GH Pages 子路徑）
+  build: { outDir: 'docs' }           // ★輸出到 docs，方便 Pages 直接吃
 })
